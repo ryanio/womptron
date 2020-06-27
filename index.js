4dc6349c59ddbea608d4843a66edf31d0dabebf4
Ryan Ghods <ryan@ryanio.com>
2020-06-27 14:15:00 -0700

    init

--- index.js
+++ index.js
@@ -1,24 +1,92 @@
 const { readFileSync, writeFileSync } = require('fs');
+const { File, FileReader } = require('file-api');
+const fetch = require('node-fetch');
+const Parser = require('rss-parser');
+const Twitter = require('twitter-lite');
 
 require.extensions['.txt'] = function (module, filename) {
   module.exports = readFileSync(filename, 'utf8');
 };
 
-const Parser = require('rss-parser');
-const fetch = require('node-fetch');
-const Twitter = require('twitter-lite');
-const FileAPI = require('file-api');
-
-const { File, FileReader } = FileAPI;
-const parser = new Parser();
 let lastWompId = Number(require('./last_womp_id.txt'));
 
-const {
-  TWITTER_CONSUMER_KEY,
-  TWITTER_CONSUMER_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET,
-} = process.env;
+const updateLastWomp = (womp) => {
+  const { id } = womp;
+  lastWompId = id;
+  writeFileSync('./last_womp_id.txt', id);
+};
+
+const secrets = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
+
+const client = new Twitter(secrets);
+
+const uploadClient = new Twitter({
+  subdomain: 'upload',
+  ...secrets,
+});
+
+const textForTweet = (womp) => {
+  const { content, location, author, playUrl } = womp;
+  return `“${content}” - at ${location} - by ${author} ${playUrl}`;
+};
+
+const base64Image = async (womp) => {
+  return await new Promise(async (resolve) => {
+    const response = await fetch(womp.imgSrc);
+    const blob = await response.blob();
+    const reader = new FileReader();
+    reader.onload = function (ev) {
+      const base64Image = ev.target.result;
+      // Format to satisfy Twitter API
+      const formattedBase64Image = base64Image.replace(
+        /^data:image\/jpeg;base64,/,
+        ''
+      );
+      resolve(formattedBase64Image);
+    };
+    reader.readAsDataURL(
+      new File({
+        name: `${womp.id}.jpg`,
+        type: 'image/jpeg',
+        buffer: Buffer.from(await blob.arrayBuffer()),
+      })
+    );
+  });
+};
+
+const tweetWomp = async (womp) => {
+  try {
+    // Fetch and upload image
+    const mediaUploadResponse = await uploadClient.post('media/upload', {
+      media_data: await base64Image(womp),
+    });
+
+    // Add alt text
+    const { content } = womp;
+    if (content && content !== '') {
+      await uploadClient.post('media/metadata/create', {
+        media_id: mediaUploadResponse.media_id_string,
+        alt_text: { text: content },
+      });
+    }
+
+    // Create tweet
+    await client.post('statuses/update', {
+      status: textForTweet(womp),
+      media_ids: mediaUploadResponse.media_id_string,
+    });
+
+    console.log(`Tweeted womp #${womp.id}: ${textForTweet(womp)}`);
+  } catch (error) {
+    console.error(error);
+  }
+  updateLastWomp(womp);
+};
 
 const truncate = (str, length = 140, ending = '…') => {
   if (str.length > length) {
@@ -28,75 +96,9 @@ const truncate = (str, length = 140, ending = '…') => {
   }
 };
 
-const updateLastWompId = (id) => {
-  lastWompId = id;
-  writeFileSync('./last_womp_id.txt', id);
-};
-
-const client = new Twitter({
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token_key: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET,
-});
-
-const uploadClient = new Twitter({
-  subdomain: 'upload',
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token_key: ACCESS_TOKEN,
-  access_token_secret: ACCESS_TOKEN_SECRET,
-});
-
-const tweetForWomp = (i) => {
-  return `“${i.content}” - at ${i.location} - by ${i.author} ${i.playUrl}`;
-};
-
-const tweetWomp = async (i) => {
-  try {
-    // Fetch image
-    const base64Image = await new Promise(async (resolve) => {
-      const response = await fetch(i.imgSrc);
-      const blob = await response.blob();
-      const reader = new FileReader();
-      reader.onload = function (ev) {
-        resolve(ev.target.result);
-      };
-      reader.readAsDataURL(
-        new File({
-          name: `${i.wompId}.jpg`,
-          type: 'image/jpeg',
-          buffer: Buffer.from(await blob.arrayBuffer()),
-        })
-      );
-    });
-
-    // Upload image
-    const mediaUploadResponse = await uploadClient.post('media/upload', {
-      media_data: base64Image.replace(/^data:image\/jpeg;base64,/, ''),
-    });
-
-    // Add alt text
-    await uploadClient.post('media/metadata/create', {
-      media_id: mediaUploadResponse.media_id_string,
-      alt_text: { text: i.content },
-    });
-
-    // Create tweet
-    await client.post('statuses/update', {
-      status: tweetForWomp(i),
-      media_ids: mediaUploadResponse.media_id_string,
-    });
-
-    updateLastWompId(i.wompId);
-    console.log(`Success! Tweeted womp #${i.wompId}: ${tweetForWomp(i)}`);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 const getWomps = async () => {
-  let feed = await parser.parseURL('https://www.cryptovoxels.com/womps.rss');
+  const parser = new Parser();
+  const feed = await parser.parseURL('https://www.cryptovoxels.com/womps.rss');
   let { items } = feed;
 
   items = items.filter((i) => Number(i.link.split('/').slice(-1)) > lastWompId);
@@ -106,8 +108,8 @@ const getWomps = async () => {
     let { link, content: rawContent, creator, title: location } = item;
 
     const linkParts = link.split('/');
-    const wompId = linkParts[linkParts.length - 1];
-    const imgSrc = `https://js.cryptovoxels.com/api/womps/${wompId}.jpg`;
+    const id = linkParts[linkParts.length - 1];
+    const imgSrc = `https://js.cryptovoxels.com/api/womps/${id}.jpg`;
 
     const coords = rawContent.match(/coords=(.+?)\'/)[1];
     const playUrl = `https://www.cryptovoxels.com/play?coords=${coords}`;
@@ -123,7 +125,7 @@ const getWomps = async () => {
     const data = await response.json();
     const author = data.avatar && data.avatar.name ? data.avatar.name : creator;
 
-    return { wompId, content, location, author, playUrl, imgSrc };
+    return { id, content, location, author, playUrl, imgSrc };
   });
 
   return await Promise.all(items);
@@ -135,8 +137,9 @@ const timeout = (ms) => {
 
 (async () => {
   const womps = await getWomps();
+  console.log(`Found ${womps.length} new womps!`);
   for (womp of womps) {
     await tweetWomp(womp);
-    await timeout(5000);
+    await timeout(7000);
   }
 })();
