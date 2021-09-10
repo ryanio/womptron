d720003ec476e0b076e4e4e83c5e01d813bfbc13
Ryan Ghods <ryan@ryanio.com>
2021-09-10 10:37:45 -0700

    update deps, add and run prettier

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { readFileSync, writeFileSync } from 'fs';
-import fetch from 'node-fetch';
-import Parser from 'rss-parser';
-import Twitter from 'twitter-lite';
+import { readFileSync, writeFileSync } from 'fs'
+import fetch from 'node-fetch'
+import Parser from 'rss-parser'
+import Twitter from 'twitter-lite'
 import { base64Image, getUniqueListBy, timeout, truncate } from './util'
 const meta = require('./meta.json')
 
@@ -22,132 +22,131 @@ const updateLastWomp = (womp: Womp) => {
   if (meta.lastWompContents.length > 5) {
     meta.lastWompContents = meta.lastWompContents.slice(1)
   }
-  writeFileSync('./meta.json', JSON.stringify(meta));
-};
+  writeFileSync('./meta.json', JSON.stringify(meta))
+}
 
 const secrets = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-};
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
 
-const client = new Twitter(secrets);
+const client = new Twitter(secrets)
 
 const uploadClient = new Twitter({
   subdomain: 'upload',
-  ...secrets,
-});
+  ...secrets
+})
 
 const textForTweet = (womp: Womp) => {
-  const { content, location, author, playUrl } = womp;
-  return `“${content}” - at ${location} - by ${author} ${playUrl}`;
-};
+  const { content, location, author, playUrl } = womp
+  return `“${content}” - at ${location} - by ${author} ${playUrl}`
+}
 
 const authorBanList = [
   'hashnews.eth' // reason: excessive spamming of duplicate content
-];
+]
 
 const tweetWomp = async (womp: Womp) => {
   if (womp.content !== '' && meta.lastWompContents.includes(womp.content)) {
-    console.log(`Skipping womp #${womp.id}: duplicate content`);
-    updateLastWomp(womp);
-    return;
+    console.log(`Skipping womp #${womp.id}: duplicate content`)
+    updateLastWomp(womp)
+    return
   }
   if (authorBanList.includes(womp.author)) {
-    console.log(`Skipping womp #${womp.id}: author banned`);
-    updateLastWomp(womp);
-    return;
+    console.log(`Skipping womp #${womp.id}: author banned`)
+    updateLastWomp(womp)
+    return
   }
   try {
     // Fetch and upload image
     const mediaUploadResponse = await uploadClient.post('media/upload', {
-      media_data: await base64Image(womp),
-    });
+      media_data: await base64Image(womp)
+    })
 
     // Add alt text
-    const { content } = womp;
+    const { content } = womp
     if (Boolean(content)) {
       await uploadClient.post('media/metadata/create', {
         media_id: mediaUploadResponse.media_id_string,
-        alt_text: { text: content },
-      });
+        alt_text: { text: content }
+      })
     }
 
     // Create tweet
     await client.post('statuses/update', {
       status: textForTweet(womp),
-      media_ids: mediaUploadResponse.media_id_string,
-    });
+      media_ids: mediaUploadResponse.media_id_string
+    })
 
-    console.log(`Tweeted womp #${womp.id}: ${textForTweet(womp)}`);
-
+    console.log(`Tweeted womp #${womp.id}: ${textForTweet(womp)}`)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 
-  updateLastWomp(womp);
-};
+  updateLastWomp(womp)
+}
 
 const getWomps = async (): Promise<Womp[]> => {
-  const parser = new Parser();
-  const feed = await parser.parseURL('https://www.cryptovoxels.com/womps.rss');
-  let { items } = feed;
+  const parser = new Parser()
+  const feed = await parser.parseURL('https://www.cryptovoxels.com/womps.rss')
+  let { items } = feed
 
   // unique
   items = getUniqueListBy(items, 'content')
 
   // since lastWompId
   items = items
-    .filter((i) => Number(i.link.split('/').slice(-1)) > meta.lastWompId)
-    .reverse();
+    .filter(i => Number(i.link.split('/').slice(-1)) > meta.lastWompId)
+    .reverse()
 
-  items = items.map(async (item) => {
-    let { link, content: rawContent, creator, title: location } = item;
+  items = items.map(async item => {
+    let { link, content: rawContent, creator, title: location } = item
 
-    const linkParts = link.split('/');
-    const id = Number(linkParts[linkParts.length - 1]);
-    const imgSrc = `https://js.cryptovoxels.com/api/womps/${id}.jpg`;
+    const linkParts = link.split('/')
+    const id = Number(linkParts[linkParts.length - 1])
+    const imgSrc = `https://js.cryptovoxels.com/api/womps/${id}.jpg`
 
-    const coords = rawContent.match(/coords=(.+?)\'/)[1];
-    const playUrl = `https://www.cryptovoxels.com/play?coords=${coords}`;
+    const coords = rawContent.match(/coords=(.+?)\'/)[1]
+    const playUrl = `https://www.cryptovoxels.com/play?coords=${coords}`
 
-    let content = rawContent.match(/<\/b>([\s\S]*?)<b>/)[1];
+    let content = rawContent.match(/<\/b>([\s\S]*?)<b>/)[1]
 
     // remove unsoliicted mentions by removing @ symbol
     content = content.replace(/(^|\s|\n)@(\w*)/, '$1')
     location = location.replace(/(^|\s|\n)@(\w*)/, '$1')
 
-    content = content.trim();
-    content = truncate(content);
+    content = content.trim()
+    content = truncate(content)
 
     const response = await fetch(
       `https://www.cryptovoxels.com/api/avatars/${creator}.json`,
       { mode: 'no-cors' }
-    );
-    const data = await response.json();
-    const author: string = data.avatar && data.avatar.name ? data.avatar.name : creator;
+    )
+    const data = await response.json()
+    const author: string =
+      data.avatar && data.avatar.name ? data.avatar.name : creator
 
     const womp = { id, content, location, author, playUrl, imgSrc }
-    return womp;
-  });
+    return womp
+  })
 
-  const womps = await Promise.all(items) as Womp[]
-  return womps;
-};
+  const womps = (await Promise.all(items)) as Womp[]
+  return womps
+}
 
-(async () => {
+;(async () => {
   console.log(`~~ womptron ~~`)
 
-  const womps = await getWomps();
+  const womps = await getWomps()
 
-  console.log(`Found ${womps.length} new womps.`);
+  console.log(`Found ${womps.length} new womps.`)
 
   for (const [index, womp] of womps.slice(0, 3).entries()) {
-    await tweetWomp(womp);
+    await tweetWomp(womp)
 
     // Wait 7s between tweets
-    if (womps[index + 1])
-      await timeout(7000);
+    if (womps[index + 1]) await timeout(7000)
   }
-})();
+})()
--- util.ts
+++ util.ts
@@ -1,41 +1,43 @@
-import fetch from 'node-fetch';
-import { File, FileReader } from 'file-api';
-import { Womp } from './index';
+import fetch from 'node-fetch'
+import { File, FileReader } from 'file-api'
+import { Womp } from './index'
 
 export async function base64Image(womp: Womp): Promise<string> {
-    return await new Promise(async (resolve) => {
-        const response = await fetch(womp.imgSrc);
-        const blob = await response.blob();
-        const reader = new FileReader();
-        reader.onload = function (ev) {
-            const base64Image = ev.target.result;
-            // Format to satisfy Twitter API
-            const formattedBase64Image = base64Image.replace(/^data:image\/jpeg;base64,/, '');
-            resolve(formattedBase64Image);
-        };
-        reader.readAsDataURL(
-            new File({
-                name: `${womp.id}.jpg`,
-                type: 'image/jpeg',
-                buffer: Buffer.from(await blob.arrayBuffer()),
-            })
-        );
-    });
-};
+  return await new Promise(async resolve => {
+    const response = await fetch(womp.imgSrc)
+    const blob = await response.blob()
+    const reader = new FileReader()
+    reader.onload = function(ev) {
+      const base64Image = ev.target.result
+      // Format to satisfy Twitter API
+      const formattedBase64Image = base64Image.replace(
+        /^data:image\/jpeg;base64,/,
+        ''
+      )
+      resolve(formattedBase64Image)
+    }
+    reader.readAsDataURL(
+      new File({
+        name: `${womp.id}.jpg`,
+        type: 'image/jpeg',
+        buffer: Buffer.from(await blob.arrayBuffer())
+      })
+    )
+  })
+}
 
 export function getUniqueListBy(arr, key) {
-    return [...new Map(arr.map(item => [item[key], item])).values()]
+  return [...new Map(arr.map(item => [item[key], item])).values()]
 }
 
 export function timeout(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 export function truncate(str: string, length = 140, ending = '…') {
-    if (str.length > length) {
-        return str.substring(0, length - ending.length) + ending;
-    } else {
-        return str;
-    }
-};
-
+  if (str.length > length) {
+    return str.substring(0, length - ending.length) + ending
+  } else {
+    return str
+  }
+}
