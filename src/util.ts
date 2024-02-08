327583980e1a44c6e0c99cfa3a0512bcee490db0
Ryan Ghods <ryan@ryanio.com>
2024-02-08 14:30:42 -0800

    fixes

--- src/util.ts
+++ src/util.ts
@@ -1,29 +1,10 @@
 import fetch from 'node-fetch'
-import { File, FileReader } from 'file-api'
 import { Womp } from './index'
 
 export async function base64Image(womp: Womp): Promise<string> {
-  return await new Promise(async (resolve) => {
-    const response = await fetch(womp.imgSrc)
-    const blob = await response.blob()
-    const reader = new FileReader()
-    reader.onload = function (ev) {
-      const base64Image = ev.target.result
-      // Format to satisfy Twitter API
-      const formattedBase64Image = base64Image.replace(
-        /^data:image\/jpeg;base64,/,
-        ''
-      )
-      resolve(formattedBase64Image)
-    }
-    reader.readAsDataURL(
-      new File({
-        name: `${womp.id}.jpg`,
-        type: 'image/jpeg',
-        buffer: Buffer.from(await (blob as any).arrayBuffer()),
-      })
-    )
-  })
+  const response = await fetch(womp.imgSrc)
+  const buf = await response.buffer()
+  return buf.toString('base64')
 }
 
 export function timeout(ms: number) {
